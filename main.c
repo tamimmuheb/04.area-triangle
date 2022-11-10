4241ecf7ce319107eccdd8e6f6cc282adc2c4d29
tamimmuheb <99010155+tamimmuheb@users.noreply.github.com>
2022-11-10 12:56:10 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//wryazahshgs
+int main()
+{
+    float base,height;
+    printf("Enter Base and Height:" );
+    scanf("%f%f",&base,&height);
+    float area = (base * height) / 2;
+
+    //Area with precision of 2 decimal places
+    printf("Area of Triangle is %0.2f",area);
+
+}
